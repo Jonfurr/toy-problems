a7c9601f23bb2908ceaae3c54cedec11c9d1ffd1
jonfurr <jonfurr@gmail.com>
2016-10-07 10:03:09 -0700

    solve alphabet_inventory

--- alphabet_inventory.js
+++ alphabet_inventory.js
@@ -7,25 +7,25 @@
 //                  "x"       | "X is my friend"       |  "x:1"            
 //                  "hue_3"   | "anlecxkac.gpaoo._43_" |  "e:1,_:2,3:1"
 
-  function alphaCount(alphabet, text) {
-  var alphabet = alphabet.toLowerCase();
-  var text = text.toLowerCase();
-  var result = [];
-  for (i=0;i<alphabet.length;i++){
-    var counter = 0;
-    for (j=0;j<text.length;j++){
-      
-      if (text[j] === alphabet[i]) {
-        counter = counter + 1;
-      }
+function alphaCount(alphabet, text) {
+    var alphabet = alphabet.toLowerCase();
+    var text = text.toLowerCase();
+    var result = [];
+    for (i = 0; i < alphabet.length; i++) {
+        var counter = 0;
+        for (j = 0; j < text.length; j++) {
+
+            if (text[j] === alphabet[i]) {
+                counter = counter + 1;
+            }
+        }
+        if (counter !== 0) {
+            result.push(alphabet[i] + ":" + counter);
+        }
     }
-    if (counter !== 0) {
-      result.push(alphabet[i]+":"+counter);
-    }
-  }
-  
-  return String(result);
+
+    return String(result);
 }
 
 
-alphaCount("abcdefghijklmnopqrstuvwxyz","Trying to test this toy problem");
+alphaCount("abcdefghijklmnopqrstuvwxyz", "Trying to test this toy problem");
